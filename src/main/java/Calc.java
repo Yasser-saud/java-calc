acfd7da1acbdedf0a1f89c5960c6623ee1bff96c
Yasser_saud <ya-1417@outlook.com>
2021-05-12 03:50:56 +0300

    initial commit for version 1, functionality to sum and subtract

---
+++ src/main/java/Calc.java
@@ -0,0 +1,14 @@
+public class Calc {
+    private static final String result = "✅ Results: ";
+
+    public void sum (float a, float b){
+        float res = a+b;
+        System.out.println(result + res);
+    }
+
+    public void subtract(float a, float b){
+        float res = a - b;
+        System.out.println(result + res);
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,92 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Calc calc = new Calc();
+        int userInput;
+        float a;
+        float b;
+        int restart;
 
+        // keep running until user wants to exit
+        while (true){
+            System.out.println("Welcome! 😊 \nPlease use the number corresponding for the action you want.");
+
+            // options
+            System.out.println(" 1 - sum two numbers \n 2 - subtract two numbers\n 3 - Exit");
+
+            // input
+            if(s.hasNextInt()){
+                userInput = s.nextInt();
+            }
+
+            else {
+                System.err.println("Error: Please enter a valid number!");
+                s.next();
+                continue;
+            }
+
+            if( userInput > 3){
+                System.err.println("The number you entered is out of the range! \n Restarting...");
+                continue;
+            }
+
+            else if( userInput == 3){
+                System.out.println("Exiting...");
+                break;
+            }
+
+            // else take input from the user
+            else {
+                // loop until a valid value is provided
+                while (true){
+                    try {
+                        System.out.println("Please enter the first number: ");
+                        a = s.nextInt();
+                        System.out.println("Please enter the second number: ");
+                        b = s.nextInt();
+                        break;
+                    }
+                    catch (InputMismatchException e){
+                        System.err.println("Error: Please enter a valid number!");
+                        s.next();
+                        continue;
+                    }
+                }
+
+                if(userInput == 1){
+                    calc.sum(a, b);
+                }
+                else{
+                    calc.subtract(a, b);
+                }
+
+
+                while (true){
+                    try {
+                        System.out.println("Want to try again? \n 1 - Yes \n Any other number - No/Exit");
+                        restart = s.nextInt();
+                        break;
+                    }
+                    catch (InputMismatchException e){
+                        s.next();
+                        continue;
+                    }
+                }
+
+                if(restart == 1){
+                    continue;
+                }
+
+                else {
+                    System.out.println("Exiting...");
+                    break;
+                }
+            }
+        }
     }
 
 }
